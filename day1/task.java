6e6d9d3c89e32af73d56cb4195faf3a9691fc3f1
maminprogrammer <maminprogrammer221@gmail.com>
2026-04-03 21:21:02 +0700

    Update task.java

--- day1/task.java
+++ day1/task.java
@@ -1,24 +1,30 @@
-// Дано:
-// список строк (имитация логов):
-//
-// List<String> logs = List.of(
-//     "INFO User logged in",
-//     "ERROR Database connection failed",
-//     "INFO Request processed",
-//     "ERROR NullPointerException",
-//     "WARN Disk almost full"
-// );
-//
-//
-// Сделать:
-// Отфильтровать только строки с "ERROR"
-// Преобразовать их в новый список, содержащий только текст ошибки (без слова ERROR)
-// Вывести результат
-//
-// Ожидаемый результат:
-//
-// Database connection failed
-// NullPointerException
-//
-// Ограничения:
-// Без циклов for (при возможности)
+import java.util.ArrayList;
+import java.util.List;
+
+public class SeqSelector {
+    public static void main(String[] args) {
+        List<String> logs = List.of(
+                "INFO User logged in",
+                "ERROR Database connection failed",
+                "INFO Request processed",
+                "ERROR NullPointerException",
+                "WARN Disk almost full"
+        );
+
+        int count = logs.size();
+        int i = 0;
+
+        List<String> resultLogs = new ArrayList<>();
+
+        //Не используем цикл for
+        while (i < count) {
+            if (logs.get(i).startsWith("ERROR")) {
+                String deleteFirstWord = logs.get(i).substring(logs.get(i).indexOf(' ') + 1);
+                resultLogs.add(deleteFirstWord);
+            }
+            i++;
+        }
+
+        resultLogs.forEach(System.out::println);
+    }
+}
