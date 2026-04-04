800b98bb859a9a54bafa122e5238cc7c0f1d17b5
antonbombov <anton.996zgr@gmail.com>
2026-04-04 21:51:04 +0700

    Update task.py

--- day4/task.py
+++ day4/task.py
@@ -1,2 +1,29 @@
 # прочитать файл access.log
 # найти строки с ERROR
+
+import logging
+import getpass
+import traceback
+
+username = getpass.getuser()
+
+logging.basicConfig(
+    level=logging.ERROR,
+    format=f'%(asctime)s - {username} - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename='errors.log',
+    encoding='utf-8'
+)
+
+try:
+    result = 1 / 0
+except ZeroDivisionError as e:
+    logging.error("Зафиксировано деление на ноль!", exc_info=True)
+
+with open('errors.log', 'r', encoding='utf-8') as file:
+    for line in file:
+        if 'ERROR' in line:
+            print(line.strip())
+
+#добавил к прошлому коду логгирование stacktrace
+#без utf8 что-то не получалось по дефолту считать данные из лог файла (вместо данных были коды ascii)
