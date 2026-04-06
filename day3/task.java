c9dd6ef52f5ff8a8616d349429528d1f86a3ef90
maminprogrammer <maminprogrammer221@gmail.com>
2026-04-06 15:47:42 +0700

    Add files via upload

--- day3/task.java
+++ day3/task.java
@@ -1,23 +1,32 @@
-// Дано:
+import java.util.*;
+import java.util.stream.Collectors;
 
-// список пользователей:
+public class task {
+    public static void main(String[] args) {
 
-// List<String> users = List.of(
-//     "alice:30",
-//     "bob:25",
-//     "charlie:35",
-//     "david:25"
-// );
+        List<String> users = List.of(
+                "alice:30",
+                "bob:25",
+                "charlie:35",
+                "david:125"
+        );
+      //  System.out.println(users);
 
-// Сделать:
+        // Используем stream api и преобразуем List в Map<String, Integer>
+        Map<String, Integer> userTable = users.stream()
+                .collect(Collectors.toMap(
+                        x -> x.split(":")[0],                    // извлекаем имя
+                        x -> Integer.parseInt(x.split(":")[1])  // извлекаем возраст и преобразуем в число
+                ));
+        //  System.out.println(userTable);
 
-// Преобразовать в Map<String, Integer> (имя → возраст)
-// Найти самого старшего пользователя
-// Вывести его имя и возраст
-// Ожидаемый результат:
-// charlie:35
 
-// Использовать:
+        Map.Entry<String, Integer> oldestPerson = userTable.entrySet().stream()
+                .max(Map.Entry.comparingByValue())  // ищем пару с максимальным возрастом
+                .orElse(null);  // если нет данных — null (но у нас есть)
+        //System.out.println(oldestPerson);
 
-// Stream API без for
-// max() или Comparator
+        System.out.println(oldestPerson.getKey() + ":" + oldestPerson.getValue());
+
+    }
+}
